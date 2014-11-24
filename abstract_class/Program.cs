3c9f0c9a11955e961b4efc9f874f18248116fe0c
guqiang180@gmail.com <ilove180>
2014-11-24 20:45:10 +0800

    testing sec

---
+++ abstract_class/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace abstract_class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p=new student();
+            p.standup();
+            Console.ReadKey();
+        }
+    }
+
+    public abstract class Person
+    {
+        public abstract void standup();
+    };
+
+    public class student:Person
+    {
+        public override void standup()
+        {
+            Console.WriteLine("laoshi hao");
+        }
+    };
+
+    public class teacher:Person
+    {
+        public override void standup()
+        {
+            Console.WriteLine("xiaozhang hao");
+            //throw new NotImplementedException();
+        }
+    };
+
+    public class headmaster:Person
+    {
+        public override void standup()
+        {
+            Console.WriteLine("xiaomeimei hao");
+            //throw new NotImplementedException();
+        }
+    };
+}
